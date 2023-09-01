8134fd4dfd2a9c549c48f796036e937b953d2c49
estzhang <Esther.Zhang@externals.adidas.com>
2023-09-01 18:16:29 +0800

    init ui

--- lib/modules/profile/profile_page.dart
+++ lib/modules/profile/profile_page.dart
@@ -1,33 +1,149 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import '../../../utils/asset_util.dart';
 
-import '../../utils/notification_controller.dart';
-import '../../routes/routes.dart';
-import '../../utils/theme_util.dart';
 
 class ProfilePage extends GetView {
   const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: GMTheme.cMainPageBackground,
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                  onPressed: () {
-                    Get.toNamed(Routes.login);
-                  },
-                  child: const Text("Login")),
-              ElevatedButton(
-                  onPressed: () {
-                    NotificationsController.createNewNotification();
-                  },
-                  child: const Text("send notification"))
-            ],
-          ),
-        ));
+    return MaterialApp(
+      title: 'Profile Challenge 01',
+      home: Scaffold(
+        body: ListView(
+          children: <Widget>[
+            SizedBox(
+              height: 350,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      CircleAvatar(
+                        radius: 16.0,
+                        backgroundImage: AssetImage(GMAsset.profileAvatar),
+                      ),
+                      SizedBox(
+                        width: 200,
+                      ),
+                      CircleAvatar(
+                        radius: 16.0,
+                        backgroundImage: AssetImage(GMAsset.profileEdit),
+                      ),
+                    ],
+                  ),
+                  const CircleAvatar(
+                      radius: 44.0,
+                      backgroundImage: AssetImage(GMAsset.profileAvatar)),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  const Text(
+                    'UserName',
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                  const Text(
+                    '@test_account',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 16,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Text(
+                    'Latest Followers',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 14,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 4,
+                  ),
+                  const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        CircleAvatar(
+                            radius: 15.0,
+                            backgroundImage: AssetImage(GMAsset.profileAvatar)),
+                        CircleAvatar(
+                            radius: 15.0,
+                            backgroundImage: AssetImage(GMAsset.profileAvatar)),
+                        CircleAvatar(
+                            radius: 15.0,
+                            backgroundImage: AssetImage(GMAsset.profileAvatar)),
+                      ]),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Container(
+                          color: Colors.white,
+                          child: const ListTile(
+                            title: Text(
+                              'Followers',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.grey,
+                              ),
+                            ),
+                            subtitle: Text(
+                              '5000',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          color: Colors.white,
+                          child: const ListTile(
+                            title: Text(
+                              'Following',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.grey,
+                              ),
+                            ),
+                            subtitle: Text(
+                              '5000',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
+
--- lib/utils/asset_util.dart
+++ lib/utils/asset_util.dart
@@ -6,4 +6,7 @@ class GMAsset {
   static const navHome = "images/nav-home.png";
   static const navCar = "images/nav-car.png";
   static const navUser = "images/nav-user.png";
+
+  static const profileAvatar = "images/profile/avatar.png";
+  static const profileEdit = "images/profile/edit.png";
 }
